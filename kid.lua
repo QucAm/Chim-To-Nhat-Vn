8360b2959f94d737e0cb126706190850f4cac1c8
QucAm <tranquocanh9022@gmail.com>
2024-10-11 21:10:46 +0700

    Create kid.lua

---
+++ kid.lua
@@ -0,0 +1,11 @@
+-- Max level, godhuman, CDK
+getgenv().Configs = {
+    ["Team"] = "Pirates",
+    ["FPS Boost"] = true,
+    ["Farm Boss Drops"] = {
+        ["Enable"] = false,
+        ["When x2 Exp Expired"] = false
+    },
+    ["Hop Player Near"] = false
+}
+loadstring(game:HttpGet("http://apixerohub.x10.mx/kaitun.lua"))()
